ed998862bb365a5d65ea323523dad93fabb29fe8
Yairb11 <yairboz70@gmail.com>
2026-06-09 20:44:10 +0300

    Added Monkey and Donut

--- MainFastCuda.py
+++ MainFastCuda.py
@@ -59,8 +59,8 @@ MAX_DEPTH = 5
 def main():
     W = 1270
     H = 720
-    name = "monkey"
-    adding_to_image = "CUDA_1"
+    name = "donut"
+    adding_to_image = ""
     camera = Camera(W, H, origin= Vector(0, 0, -3), target=Vector(0, 0, -2), up_vector=Vector(0, 1, 0), FOV=90)
     lights = [Light(Vector(0,0,-4), 1)]
     start_engine(W, H, camera, lights, name, adding_to_image)
